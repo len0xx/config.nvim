c4f03a208e70189819b30c42bd9d016f5268834d
Prokhor Minin <prokhor@len0xx.space>
2023-03-22 17:31:40 +0500

    Support for Vue

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -86,6 +86,18 @@ lspconfig.svelte.setup {
 	on_attach = on_attach,
 }
 
+
+lspconfig.volar.setup {
+    capabilities = capabilities,
+	on_attach = on_attach,
+	init_options = {
+		typescript = {
+			tsdk = "/opt/homebrew/lib/node_modules/typescript/lib"
+			-- tsdk = "/usr/local/lib/node_modules/typescript/lib"
+		}
+	}
+}
+
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         update_in_insert = true,
