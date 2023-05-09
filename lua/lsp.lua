bc864ace417c0fe27bb0fd4d74c22c3032f3dc04
Prokhor Minin <prokhor@len0xx.space>
2023-05-09 14:41:00 +0500

    Support for C/C++ and remap completion abort to Escape

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,25 +1,25 @@
 local on_attach = function(_client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- lspconfig
@@ -27,26 +27,35 @@ local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+lspconfig.clangd.setup{
+	capabilities = capabilities,
+	on_attach = on_attach,
+}
+
 lspconfig.html.setup {
-    cmd = { "vscode-html-language-server", "--stdio" },
-    capabilities = capabilities,
+	cmd = { "vscode-html-language-server", "--stdio" },
+	capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 lspconfig.rust_analyzer.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 lspconfig.cssls.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 lspconfig.phpactor.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
-	root_dir = lspconfig.util.root_pattern("composer.json", ".git", "index.php")
+	root_dir = lspconfig.util.root_pattern("composer.json", ".git", "index.php"),
+	init_options = {
+		["language_server_phpstan.enabled"] = false,
+		["language_server_psalm.enabled"] = false
+	}
 }
 
 lspconfig.lua_ls.setup {
@@ -60,14 +69,14 @@ lspconfig.lua_ls.setup {
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
-				globals = {'vim'},
+				globals = { 'vim' },
 			}
 		}
 	}
 }
 
 lspconfig.pyright.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
 }
 
@@ -79,19 +88,19 @@ lspconfig.tsserver.setup {
 }
 
 lspconfig.denols.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
 	root_dir = lspconfig.util.root_pattern("deno.json")
 }
 
 lspconfig.svelte.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 
 lspconfig.volar.setup {
-    capabilities = capabilities,
+	capabilities = capabilities,
 	on_attach = on_attach,
 	init_options = {
 		typescript = {
@@ -102,21 +111,20 @@ lspconfig.volar.setup {
 }
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        update_in_insert = true,
-    }
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		update_in_insert = true,
+	}
 )
 
 lspconfig.sqls.setup {
-    settings = {
-        sqls = {
-            lowercaseKeywords = true,
-            connections = {}
-        }
-    },
-    capabilities = capabilities,
-    on_attach = function(client, buffer)
-        require('sqls').on_attach(client, buffer)
-    end
+	settings = {
+		sqls = {
+			lowercaseKeywords = true,
+			connections = {}
+		}
+	},
+	capabilities = capabilities,
+	on_attach = function(client, buffer)
+		require('sqls').on_attach(client, buffer)
+	end
 }
-
