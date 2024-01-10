507bbc3369287831665c900881473f1f53e7ef1c
Prokhor Minin <len0xx@icloud.com>
2024-01-10 12:14:27 +0500

    Fix install-lazygit-linux-x86_64.sh

--- install-lazygit-linux-x86_64.sh
+++ install-lazygit-linux-x86_64.sh
@@ -2,6 +2,6 @@
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo $HOME/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf $HOME/lazygit.tar.gz lazygit
+tar -C $HOME -xf $HOME/lazygit.tar.gz lazygit
 sudo install $HOME/lazygit /usr/local/bin
 rm $HOME/lazygit $HOME/lazygit.tar.gz
