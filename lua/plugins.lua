9da1c26025d08052bab999f1d28bac42e5ee25b9
Prokhor <len0xx@icloud.com>
2024-04-18 18:47:10 +0500

    Install nvim-tree from master branch

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,8 +14,7 @@ return require('packer').startup(function(use)
         'nvim-tree/nvim-tree.lua',
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+        }
     }
 
     -- tabs and buffer management
